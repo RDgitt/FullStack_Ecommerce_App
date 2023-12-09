fbc3da249ce254701a40adbb9f3a14299357f20f
rakeshDev <rakeshdabhade188@gmail.com>
2023-12-09 23:53:23 +0530

    Completed products get api

---
+++ backend/products.js
@@ -0,0 +1,25 @@
+const produts = [
+  {
+    id: 1,
+    name: "iPhone 15 Pro",
+    desc: "6.1-inch display",
+    price: 999,
+    image: "https://res.cloudinary.com/djadmwnbb/image/upload/v1702146003/EcommerceApp/iphone-15-pro_qb2hl4.jpg",
+  },
+  {
+    id: 2,
+    name: "Samsung 16 Pro",
+    desc: "7.1-inch display",
+    price: 1999,
+    image: "https://res.cloudinary.com/djadmwnbb/image/upload/v1702145984/EcommerceApp/Erstes_Sample_Photo_aus_der_Samsung_Galaxy_S22_zzcelt.jpg",
+  },
+  {
+    id: 3,
+    name: "Realme 17 Pro",
+    desc: "8.1-inch display",
+    price: 2999,
+    image: "https://res.cloudinary.com/djadmwnbb/image/upload/v1702146011/EcommerceApp/Realme_11_pro_atmgzv.webp",
+  },
+];
+
+module.exports = produts;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
-const express = require('express');
-
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config();
-
+require("dotenv").config();
+const products = require('./products')
 const port = process.env.PORT || 9000; // You can choose any available port
 
+app.use(express.json());
+app.use(cors());
+
 // Define a route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/products", (req, res) => {
+  res.send(products);
 });
 
 // Start the server
